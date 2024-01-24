1d89b722500a78596c86247175613b342bb6060f
gangriver <shinker01@gmail.com>
2024-01-24 14:03:03 +0900

    commit

---
+++ Java_20240122_제어문/src/ex04/로또03.java
@@ -0,0 +1,30 @@
+package ex04;
+
+public class 로또03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] lotto = new int[45];
+
+		for (int j = 0; j < 5; j++) {
+			for (int i = 0; i < lotto.length; i++)
+				lotto[i] = i + 1;
+
+			for (int i = 0; i < 1000; i++) {
+				int num = (int) (Math.random() * 44) + 1;
+				int tmp = lotto[0];
+				lotto[0] = lotto[num];
+				lotto[num] = tmp;
+			}
+
+			for (int i = 0; i < 6; i++)
+				System.out.print(lotto[i] + " ");
+
+			System.out.println();
+			System.out.println("\n----------------------------");
+
+		}
+
+	}
+}
---
+++ java.20240119_연산자/src/ex02/scanner01.java
@@ -0,0 +1,21 @@
+package ex02;
+
+import java.util.Scanner;
+
+public class scanner01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("당신의 나이는 : ");
+		int age = input.nextInt();
+		
+		System.out.print("당신의 이름을 입력하세요");
+		input.nextLine();
+		String name = input.nextLine();
+
+
+		System.out.println("이름은 " + name + " 나이는 "+age + "이요");
+	}
+}
