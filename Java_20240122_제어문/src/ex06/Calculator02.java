b23e4ba72a206b26a4bc33b1b77e4d906f85e7cb
gangriver <shinker01@gmail.com>
2024-01-26 17:58:18 +0900

    commit

--- Java_20240122_제어문/src/ex06/Calculator02.java
+++ Java_20240122_제어문/src/ex06/Calculator02.java
@@ -1,26 +1,41 @@
 package ex06;
 
-public class Calculator {
+public class Calculator02 {
+	
+		public static void main(String[] args) {
+			
+		
 	//리턴값이 없는 메소드 선언
-	void powerOn() {
-		System.out.println("전원을 켭니다.");
-	}
+	 	powerOn();
+		powerOff();
+		
+		int num = plus(5,2);
+		System.out.println(num);
+		
+		double avg = divide(5,2);
+		System.out.println(avg);
+	}		
 	
 	//리턴값이 없는 메소드 선언
-	void powerOff() {
+		static void powerOn() {
+			System.out.println("전원을 켭니다.");
+		}
+	
+	//리턴값이 없는 메소드 선언
+	static void powerOff() {
 		System.out.println("전원을 끕니다.");
 	}
 	
 	//호출 시 두 정수 값을 전달받고,
 	//호출한 곳으로 결과값 int를 리턴하는 메소드 선언
-	int plus(int x, int y) {
+	static int plus(int x, int y) {
 		int result = x + y;
 		return result; //리턴값 지정;
 	}
 	
 	//호출 시 두 정수 값을 전달받고,
 	//호출한 곳으로 결과값 double을 리턴하는 메소드 선언
-	double divide(int x, int y) {
+	static double divide(int x, int y) {
 		double result = (double)x / (double)y;
 		return result;  //리턴값 지정
 	}
