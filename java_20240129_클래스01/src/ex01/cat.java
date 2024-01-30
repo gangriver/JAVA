52181ea35ab8c55cd9e3e4b65b9a954bdf2998f8
gangriver <shinker01@gmail.com>
2024-01-30 10:53:16 +0900

    commit

--- java_20240129_클래스01/src/ex01/cat.java
+++ java_20240129_클래스01/src/ex01/cat.java
@@ -3,10 +3,16 @@ package ex01;
 public class cat {
 
 		//멤버 변수 --재료
-		private String breed;
-		private String color;
+		String breed;
+		String color;
+		int age;
 		
-		
+		void setAge(int n) {
+			if(n>0 && n<=150)
+				age = n;
+			else 
+				age = 1;
+		}
 		//생성자(클래스이름하고 같음) --반환타입x
 		cat() {
 			
@@ -17,7 +23,7 @@ public class cat {
 		//void(반환타입) eat(함수명) ()(매개변수)
 		//멤버 메소드 --액션
 		void eat() {
-			
+			System.out.println("age");
 			System.out.println("츄르를 먹는다");
 			System.out.println("칼라는 " + color + "이다.");
 		}
--- java_20240129_클래스01/src/ex01/exalmple01.java
+++ java_20240129_클래스01/src/ex01/exalmple01.java
@@ -11,17 +11,19 @@ public class exalmple01 {
 		cat c1 = new cat();
 		//--> new cat()란 생성자 호출을 통해서 객체를 생성한다.
 
-//		c1.breed = "페르시안고양이";
-//		c1.color = "흰색";
-		
+		c1.breed = "페르시안고양이";
+		c1.color = "흰색";
+//		c1.setAge(100);
+		c1.age=1000;
+		c1.eat();
 		
 		
 		c1.eat();
 		
 		cat c2 = new cat();
 		
-//		c2.breed = "코리아숏트헤어고양이";
-//		c2.color = "검은색";
+		c2.breed = "코리아숏트헤어고양이";
+		c2.color = "검은색";
 		
 		c2.eat();
 	}
